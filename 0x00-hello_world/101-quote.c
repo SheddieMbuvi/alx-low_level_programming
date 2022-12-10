e8fba07fca647437037511b784bad712fc9dfc1c
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-10 07:32:38 -0800

    Size output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,13 @@
  */
 int main(void)
 {
+	FILE *fp;
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	
-	fwrite(str, sizeof(str));
+
+	fp = fopen("file.txt", "w");
+	fwrite(str, 1, sizeof(str), fp);
+
+	fclose(fp);
+
 	return (1);
 }
