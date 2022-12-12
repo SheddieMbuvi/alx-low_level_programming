6088ff8320c772fc1a0b8a212c19f5dcdfaf1528
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-12 02:16:55 -0800

    Unix is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * Main - Entry point
- * Return: Always 1 (Success)
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
  */
 int main(void)
 {
-	FILE *fp;
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str;
 
-	fp = fopen("101-quote.c", "w");
-	fwrite(str, 1, sizeof(str), fp);
+		str == "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	putchar(str);
 
-	return (1);
+	return (0);
 }
