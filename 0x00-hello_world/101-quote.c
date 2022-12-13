18842183b1ec4c14a6b4d3e7c1b8748ac58d8075
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-12 22:55:19 -0800

    unix is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	char STDOUT_FILENO;
 
-	fwrite(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	putchar(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
 	return (0);
 }
