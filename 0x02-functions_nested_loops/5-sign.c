512ff7b0ea818329d7ad83a260c7715e70bf1678
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-14 07:12:57 -0800

    Print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - show te she sign
+ *
+ * @n: number to be checked
+ *
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		x = 1;
+		_putchar('+');
+	}
+	else if (x == 0)
+	{
+		x = 0;
+		_putchar('0');
+	}
+	else
+		x = -1;
+	_putchar('-');
+
+	return (x);
+}
