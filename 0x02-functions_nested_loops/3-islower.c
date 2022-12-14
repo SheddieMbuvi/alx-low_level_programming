c49bde58e0104bcc7ef491a95ea4b570c94ec630
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-14 06:33:59 -0800

    Islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _islower - function entry point
+ * _islower - checks if a character is lowercase function entry point
  * Compaare if a character is small or a capital letter
+ * @c: is th integer value to be compared with ASCII value
  * Return: Always 0 (iscapital) or 1 (lower)
  */
 int _islower(int c)
