5e0ce2ce9fe5326bd843ade8d29468606d81a264
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-14 06:47:08 -0800

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdioh.h>
+#include "main.h"
+/**
+ * _isalpha - Function for leters either lower or upper
+ * @c: you input a character
+ *
+ * Return: 0 if letter capital or small otherwise return 1
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
