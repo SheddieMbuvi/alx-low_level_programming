640876257f7cc593ee297785125a07a4d8f6d6cf
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-14 08:14:58 -0800

    Absolete

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - Function to compute absoulute value of intenger
+ *
+ * @c: complie the programm
+ * Return: 0 success
+ */
+int _abs(int)
+{
+	int i;
+
+	i = abs(int);
+}
