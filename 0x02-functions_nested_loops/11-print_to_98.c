3b2f85d4c8c07302e6e360dc1a863b8110d81c30
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-15 04:29:56 -0800

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,20 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	for (i = n; i <= 98; i++)
+	if (n < 98)
 	{
-		_putchar(n);
-		_putchar(',');
-		_putchar(' ');
+		for (n = n; n < 98; n++)
+		{
+			printf("%d, ", n);
+			printf("%d\n", 98);
+		}
 	}
-	_putchar('\n');
+	else
+	{
+		for (n = n; n> 98; n--)
+		{
+			printf("%d, ", n);
+			printf("%d\n", 98);
+		}
+	}
 }
