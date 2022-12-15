ae73fa7fa321c741aa5f3c5603609c2511808406
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-15 02:43:31 -0800

    Sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return:  Always return 0
+ */
+int add(int, int)
+{
+	int a, b, sum;
+
+	sum = a + b;
+
+	return (sum);
+}
