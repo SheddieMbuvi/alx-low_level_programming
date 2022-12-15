ca090f4e2c78eafba47aab1633371813ddb6e9fa
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-15 04:54:31 -0800

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,9 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-		for (n = n; n <= 98; n++)
+		for (n = n; n < 98; n++)
 		{
 			if (n == 98)
 			{
@@ -22,7 +22,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n = n; n >= 98; n--)
+		for (n = n; n > 98; n--)
 		{
 			if (n == 98)
 			{
