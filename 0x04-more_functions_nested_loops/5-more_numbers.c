8db7a3b366eeef025f71001de2ffb33034a05cb8
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-16 06:12:53 -0800

    print ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,20 +8,22 @@
  */
 void more_numbers(void)
 {
-	int j;
+	int j = 0;
 
-	while (j = 0; j < 11; j++)
+	while (j < 11)
 	{
-		int i;
+		int i =  0;
 		
-		for (i = 0; i < 15; i++)
+		for (i < 15)
 		{
-			if (i > 10)
+			if (i >= 10)
 			{
-			_putchar((i / 10) + 48);
+				_putchar((i / 10) + 48);
 			}
 			_putchar((i % 10) + 48);
+			i++;
 		}
+		j++;
 		_putchar('\n');	
 	}
 }
