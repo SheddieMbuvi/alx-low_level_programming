11532ee4eef1da4b3e57f6cc23723ffc64814503
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-16 03:56:52 -0800

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * i: the characters to be printed
+ *
+ * Return: 1 success else 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (i);
+}
