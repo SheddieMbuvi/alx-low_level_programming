b29a3f80dfb5a4ec3a1cf18c50e19890bc7435bb
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-16 07:28:31 -0800

    Square printed

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "mine.h"
+/**
+ * print_square - funtion printing a square
+ * @b: input integer
+ * Return: 0 success
+ */
+void print_square(int b)
+{
+	int i, j;
+
+	if (b > 0)
+	{
+		for (i = 0; i < b; i++)
+		{
+			for (j = 0; j < b; j++)
+				_putchar('#');
+			putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
