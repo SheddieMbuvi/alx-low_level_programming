4721b46f52cb96122d6523bd41c9406d31604ebc
SheddieMbuvi <sheddiembuvi@gmail.com>
2022-12-16 03:38:14 -0800

    product of two

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Function to find the thr product
+ * @a: first character
+ * @b: second character
+ * Return: 1 sucess
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return(c);
+}
