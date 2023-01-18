6ce2004ae30e48fe9e272b44adda2ed620c60b3f
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-01-18 06:46:47 -0800

    Dog Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Struct for dog
+ * @name: Dog name
+ * @age: Dog's age
+ * @owner: The owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
