0216e7034868a0d41447a8f5779598df570f125b
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-04-07 12:07:32 -0700

    Header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,5 @@
-#ifndef _HASH_TABLES_H_
-#define _HASH_TABLE_H_
+#ifndef HASH_TABLES_H
+#define HASH_TABLE_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -32,3 +32,13 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 }hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
+#endif
